829af70916b6206cafdcc91fb87f303b7386c80c
Egor <egorkubarev096@gmail.com>
2024-05-21 10:20:09 +0300

    The first pagination option

--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,5 @@
 from unicodedata import category
+from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from goods.models import Products
@@ -6,16 +7,19 @@ from goods.models import Products
 # Create your views here.
 
 
-def catalog(request,category_slug):
+def catalog(request,category_slug,page=1):
 
     if category_slug == 'all':
          goods= Products.objects.all()
     else:
          goods=get_list_or_404(Products.objects.filter(category__slug=category_slug))
-         
+
+    paginator= Paginator(goods, 3)  # выводит по три товара на стр
+    current_page = paginator.page(page) #номер страницы
     context = {
         "title": "Home - каталог",
-        "goods": goods 
+        "goods": current_page, #возвращает query-set урезанный до 3
+        "slug_url": category_slug
         
     }
     return render(request, "goods/catalog.html",context)
