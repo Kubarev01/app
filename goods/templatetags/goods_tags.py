6a2db6f0a8c2b20fa384ff5bffceae198f0fc947
Egor <egorkubarev096@gmail.com>
2024-06-01 11:49:29 +0300

    Full text search well done

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,7 @@
-
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+import re
 from django import template
 from django.utils.http import urlencode
 
@@ -17,3 +20,11 @@ def change_params(context, **kwargs):
     query = context['request'].GET.dict()
     query.update(kwargs)
     return urlencode(query)
+
+@register.filter(name='highlight')
+@stringfilter
+def highlight(text, words):
+    words = words.split()
+    for word in words:
+        text = re.sub(r'(%s)' % re.escape(word), r'<mark>\1</mark>', text, flags=re.IGNORECASE)
+    return mark_safe(text)
